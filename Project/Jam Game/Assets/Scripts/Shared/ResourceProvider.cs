7fa76ed7e07bc97518fdba44996f4c1c023accfd
wy8933 <wy8933@rit.edu>
2024-08-17 11:22:12 -0400

    Added fuel gain when asteroid is destroyed

--- Project/Jam Game/Assets/Scripts/Shared/ResourceProvider.cs
+++ Project/Jam Game/Assets/Scripts/Shared/ResourceProvider.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ResourceDrop { 
+    fuel
+}
+
 public class ResourceProvider : MonoBehaviour
 {
+    public ResourceDrop resourceDrop;
+    public float resourceMultiplier;
     private void OnDestroy()
     {
         //provide material
+        if (resourceDrop == ResourceDrop.fuel) {
+            ResourceManager.Instance.UpdateFuel(5 * resourceMultiplier);
+        }
     }
 }
