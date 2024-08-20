bb0e43d0482a448e7707307462232c9ad46127ef
wy8933 <wy8933@rit.edu>
2024-08-20 09:28:09 -0400

    Added election messages and goodwill manager

--- Project/Jam Game/Assets/Scripts/Ship/GoodWillManager.cs
+++ Project/Jam Game/Assets/Scripts/Ship/GoodWillManager.cs
@@ -2,17 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoodWillManager : MonoBehaviour
+public class GoodWillManager : MonoBehaviourSingleton<GoodWillManager>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float timer;
+    public float Goodwill;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= 1.0f) {
+            Goodwill += Settings.Instance.GoodWillMultiplier;
+            timer -= 1;
+        }
+
+        UIManager.Instance.goodwillValue.text = Goodwill.ToString();
     }
 }
